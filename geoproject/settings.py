3cb72b4a8cbcc23d041e2e20dd3cc8a0515716e4
Valery <valerylukin@yahoo.com>
2016-07-13 19:57:19 +0600

    final geoproject

--- geoproject/settings.py
+++ geoproject/settings.py
@@ -132,10 +132,14 @@ MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", 
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "staticfiles")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), "static"),
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
