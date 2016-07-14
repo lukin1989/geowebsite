6ddecae6c133e2ac7f6f11fb2c254c18a6eab2d5
Valery <valerylukin@yahoo.com>
2016-07-14 17:51:41 +0600

    another geo

--- geoproject/wsgi.py
+++ geoproject/wsgi.py
@@ -1,13 +1,14 @@
 """
-WSGI config for geoproject project.
-
+WSGI config for myproject project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+import os
 
-application = Cling(get_wsgi_application())
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+application = get_wsgi_application()
